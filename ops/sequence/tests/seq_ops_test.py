4698770bb5dd0285629b4f390d9028bb4007a5bb
delzac <delzac.jh@gmail.com>
2018-12-07 22:43:35 +0800

    seq pad test improved

--- ops/sequence/tests/seq_ops_test.py
+++ ops/sequence/tests/seq_ops_test.py
@@ -18,14 +18,16 @@ def test_sequence_length():
 
 def test_sequence_pad():
     pattern = (1, 1)
+    input_dim = 1
     s1 = 5
     s2 = 3
-    a = C.sequence.input_variable(1)
+    a = C.sequence.input_variable(input_dim)
     b = pad(a, pattern)
     c = length(b)
-    n1 = np.random.random((s1, 1)).astype(np.float32)
-    n2 = np.random.random((s2, 1)).astype(np.float32)
+    n1 = np.random.random((s1, input_dim)).astype(np.float32)
+    n2 = np.random.random((s2, input_dim)).astype(np.float32)
 
     result = c.eval({a: [n1, n2]})
     assert result[0][0] == s1 + sum(pattern), result[0][0]
     assert result[1][0] == s2 + sum(pattern), result[0][0]
+    assert b.shape == (input_dim, )
